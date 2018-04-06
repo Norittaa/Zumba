6d3edec690c77e6f037f907f6cd4170c1faf0f15
Norittaa <36974945+Norittaa@users.noreply.github.com>
2018-04-05 17:14:37 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
-$.getJSON( "https://api.airtable.com/v0/appmZrzSVYBsjqjl9/zumba%20locations?api_key=keyCY2J3kHTO3SsDs", function( data ) {
-  // console.log(data.records);
+$.getJSON( "https://api.airtable.com/v0/appmZrzSVYBsjqjl9/zumba%20students?api_key=keyCY2J3kHTO3SsDs", function( data ) {
+console.log(data.records);
  var items = [];
   $.each( data.records, function( index, val ) {
-
+   
     items.push( "<li id='" + val.id + "'>" + val.fields["Name"] + "</li>" );
   });
-
+ 
   $( "<ol/>", {
     "class": "my-new-list",
     html: items.join( "" )
